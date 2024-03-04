0fb40001c5db31e42dbdb0e8fef240502da5dfa7
VladBryzhak <124492057+VladBryzhak@users.noreply.github.com>
2024-03-04 18:20:16 +0200

    finale-code

--- files/person/person.js
+++
@@ -1,7 +0,0 @@
-function greet(person) {
-  return 'Hello, ' + person.firstName + ' ' + person.lastName + '!:)';
-}
-
-let user = { firstName: 'Vlad', lastName: 'Bryzhak' };
-
-console.log(greet(user)); 
--- files/sum/addNumbers.ts
+++
@@ -1,5 +0,0 @@
-function addnumber(num1 : number, num2 : string) {
-  return num1 + num2;
-}
-
-addnumber(1, '1');
